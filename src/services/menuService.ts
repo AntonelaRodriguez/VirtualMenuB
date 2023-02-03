130473acad7ccaa097dc63f1831b85b5b700e256
AntonelaRodriguez <antonelabelenrodriguez@gmail.com>
2023-02-02 21:56:00 -0300

    first commit

---
+++ src/services/menuService.ts
@@ -0,0 +1,5 @@
+import menuData from './menu.json'
+
+export const getEntries = () => menuData
+
+export const addEntries = () => null
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+export interface MenuEntry {
+    id: number,
+    name: string,
+    type: string,
+    ingredients: Array,
+    price: number
+} 
