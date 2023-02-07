b3fe2ec018e4611297c4a68f07b6eee3682fc097
AntonelaRodriguez <antonelabelenrodriguez@gmail.com>
2023-02-07 18:44:03 -0300

    Order

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv';
+import menu from './src/routes/menu'
+
 const app = express()
 app.use(express.json()) // middleware que transforma la req.body a un json
-import menu from './routes/menu'
 
-const PORT = 3000
+dotenv.config();
+const port = process.env.PORT;
 
 app.get('/ping', (_req, res) => {
     console.log('someone pinged here!!')
@@ -12,6 +15,6 @@ app.get('/ping', (_req, res) => {
 
 app.use('/api/menu', menu)
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 }) 
--- src/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface MenuEntry {
-    id: number,
-    name: string,
-    type: string,
-    ingredients: Array,
-    price: number
-} 
